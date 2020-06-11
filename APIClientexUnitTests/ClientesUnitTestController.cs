cf8e508d887ce3e81541d9246f3bf39a91c1bf48
Leandro Brum <leandrocsantana@yahoo.com.br>
2020-06-11 11:41:23 -0300

    xUnit e Fluent Assertion - Primeiros testes

---
+++ APIClientexUnitTests/ClientesUnitTestController.cs
@@ -0,0 +1,32 @@
+﻿using APICliente.Context;
+using APICliente.Controllers;
+using APICliente.Models;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using Xunit;
+
+namespace APIClientexUnitTests
+{
+    public class ClientesUnitTestController
+    {
+
+        private readonly AppDbContext context;
+
+        [Fact]
+        public void GetClientes_Return_OkResult()
+        {
+
+        //Arrange
+        var controller = new ClientesController(context);
+
+        //Act
+        var data = controller.Get();
+
+        //Assert
+        Assert.IsType<List<Cliente>>(data.Value);
+
+        }
+
+}
+}
---
+++ APIClientexUnitTests/DBUnitTestsMockInitializer.cs
@@ -0,0 +1,26 @@
+﻿using APICliente.Context;
+using APICliente.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIClientexUnitTests
+{
+    class DBUnitTestsMockInitializer
+    {
+        public DBUnitTestsMockInitializer()
+        {
+        }
+
+        public void Seed(AppDbContext context)
+        {
+            context.Clientes.Add(new Cliente { ClienteId = 1, Cpf = "111.222.333.444-55", DataNascimento = Convert.ToDateTime("1981-01-01"), Nome = "Alan"  });
+            context.Clientes.Add(new Cliente { ClienteId = 2, Cpf = "222.333.444.555-66", DataNascimento = Convert.ToDateTime("1982-02-02"), Nome = "Bruna"  });
+            context.Clientes.Add(new Cliente { ClienteId = 3, Cpf = "333.444.555.666-77", DataNascimento = Convert.ToDateTime("1983-03-03"), Nome = "Celia"  });
+            context.Clientes.Add(new Cliente { ClienteId = 4, Cpf = "444.555.666.777-88", DataNascimento = Convert.ToDateTime("1984-04-04"), Nome = "Daniele"  });
+            context.Clientes.Add(new Cliente { ClienteId = 5, Cpf = "555.666.777.888-99", DataNascimento = Convert.ToDateTime("1985-05-05"), Nome = "Elias"  });
+
+            context.SaveChanges();
+        }
+    }
+}
